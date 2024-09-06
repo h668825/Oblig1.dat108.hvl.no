93872e103bad59952552f59e1fd4cc30a5a23f8c
Sairtoume <themathiastefre@gmail.com>
2024-09-06 08:06:11 +0200

    lagde Comparator og forenkla utskrift på oppgåve 1a

--- src/Oppg1a.java
+++ src/Oppg1a.java
@@ -1,19 +1,17 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Oppg1a {
 
 	public static void main(String[] args) {
 		List<String> listen = Arrays.asList("10", "1", "20", "110", "21", "12");
 		
-		// liste skal sorterast og skrivast ut? 
-		//Skriv ned oppgåveteksten her så det er lettare å skjønne.
-		listen.stream()
-			.mapToInt(x -> Integer.parseInt(x))
-			.sorted()
-			.forEach( s -> System.out.print(s + ","));
-			
-			// TODO konkatenere som streng, fjerne komma til slutt	
-			// TODO lag eit predikat som skal sorterast med
+		// Oppgåve: skrive ut liste på skjermen sortert etter tallverdi
+		// skal bruke Collections.sort()
+		// lambda-uttrykk for Comparator som sort() nyttar
+		System.out.println(listen.stream()
+			.sorted((a, b) -> Integer.parseInt(a) - Integer.parseInt(b))
+			.collect(Collectors.toList()));
 	}
 }
--- src/Oppg1b.java
+++ src/Oppg1b.java
@@ -1,35 +1,28 @@
+import java.util.function.BinaryOperator;
 
 public class Oppg1b {
 	
 	public static void main(String[] args) {
 		
-		int svar;
-		
-		//tips - Bifunction / binary operator
+		int svar;		
 		
 		// i)
-		Beregner sum = (a, b) -> a + b;
+		BinaryOperator<Integer> sum = (a, b) -> a + b;
 		svar = beregn(12, 13, sum );
 		System.out.println("summen av 12 og 13 er lik " + svar);
 		
 		// ii)
-		Beregner storst = (a, b) -> Math.max(a, b);
+		BinaryOperator<Integer> storst = (a, b) -> Math.max(a, b);
 		svar = beregn(-5, 3, storst );
 		System.out.println(svar + " er størst av -5 og 3");
 		
 		// iii)
-		Beregner avstand = (a, b) -> Math.abs(a - b);
+		BinaryOperator<Integer> avstand = (a, b) -> Math.abs(a - b);
 		svar = beregn(54, 45, avstand);
 		System.out.println("avstanden mellom 54 og 45 er " + svar);
 	}
 	
-	public static int beregn(int a, int b, Beregner r) {
-		return r.run(a, b);
+	public static int beregn(int a, int b, BinaryOperator<Integer> r) {
+		return r.apply(a, b);
 	}
 }
-
-@FunctionalInterface
-interface Beregner {
-	public int run(int a, int b);
-}
-
